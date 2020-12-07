98db7e568dce8d6809486fe8dfbf50b6b4432df1
TristanAppDev <20626973+TristanAppDev@users.noreply.github.com>
2020-12-07 09:52:20 +0100

    solution

--- Haskell/proplem_1.hs
+++ Haskell/proplem_1.hs
@@ -1,7 +1,7 @@
-import Data.List (union)
+-- My solution for Project Euler - Problem 1
 
 main :: IO ()
-main = print problemSolver2
+main = print problemSolver
 
-problemSolver1 = sum ([3,6..999] `union` [5,10..999])
-problemSolver2 = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+problemSolver :: Integer
+problemSolver = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
